81eb23b0ed1cecea7080ffd538edeafdc3a99a80
Alexander Lavrenchuk <lavrenchuk@yandex.ru>
2025-02-27 18:14:00 +0300

    edit make_cf.py - export DataFrame with CF to excel file without formating cells

--- make_cf.py
+++ make_cf.py
@@ -10,6 +10,16 @@ from openpyxl.utils import get_column_letter
 from collections.abc import Callable
 
 
+def move_column(data_frame: pd.core.frame.DataFrame,
+    column_name: str, loc: int):
+
+    col = data_frame.pop(column_name)
+    data_frame.insert(
+        loc=loc,
+        column=column_name,
+        value=col)
+
+
 def select_sum_amount_group_by_entity(table_name: str) \
         -> pd.core.frame.DataFrame:
 
@@ -144,48 +154,136 @@ def select_sum_amount_group_by_account_entity(table_name: str) \
 
     sql_query = f'''
         SELECT
-            t0.period_name,
-            t1.account_id,
-            t1.entity_id,
-            SUM(t1.amount) AS amount_sum,
-            t2.amount_sum_by_account,
-            t3.amount_sum_by_account_entity
-        FROM periods AS t0
-        LEFT JOIN {table_name} AS t1
-        ON t0.period_name = t1.period_name
-        LEFT JOIN (
-            SELECT account_id, SUM(amount) AS amount_sum_by_account
-            FROM {table_name}
-            GROUP BY account_id) AS t2
-        ON t1.account_id = t2.account_id
-        LEFT JOIN (
-            SELECT
-                account_id,
-                entity_id,
-                SUM(amount) AS amount_sum_by_account_entity
-            FROM {table_name}
-            GROUP BY account_id, entity_id) AS t3
-        ON t1.account_id = t3.account_id AND t1.entity_id = t3.entity_id
-        GROUP BY t0.period_name, t1.account_id, t1.entity_id
-        ORDER BY
-            t2.amount_sum_by_account DESC,
-            t3.amount_sum_by_account_entity DESC;
-#         (SELECT
-#             t1.period_name,
-#             t1.account_id AS `account_id`,
-#             t3.account_name AS `account_name`,
-#             t4.entity_1C_name AS `entity_name`,
-#             t4.inn AS `inn`,
-#             SUM(t1.amount) AS `{table_name}`
-#         FROM {table_name} AS t1
-#         JOIN accounts AS t3 ON t3.id = t1.account_id
-#         JOIN entities AS t4 ON t4.id = t1.entity_id
-#         GROUP BY t1.period_name, t1.account_id, t1.entity_id
-#         ORDER BY t1.account_id, t1.entity_id);
+            id,
+            account_name
+        FROM accounts;
         '''
+    df_accounts = select_data(sql_query)
+    df_accounts.set_index(
+        keys='id',
+        inplace=True)
+    
+    sql_query = f'''
+        SELECT
+            id,
+            entity_1C_name,
+            inn
+        FROM entities;
+        '''
+    df_entities = select_data(sql_query)
+    # df_entities.set_index(
+    #     keys='id',
+    #     inplace=True)
+    df_entities.inn = df_entities.inn.astype('str')
+    df_entities.inn = df_entities.inn.str.replace('.0', '')
+    df_entities.inn = df_entities.inn.str.replace('nan', '')
+    # print(df_entities)
+    
+    sql_query = f'''
+        SELECT
+            period_name,
+            account_id,
+            entity_id,
+            SUM(amount) AS amount_sum
+        FROM {table_name}
+        GROUP BY period_name, account_id, entity_id;
+        '''
+    df_amounts = select_data(sql_query)
+    
+    sql_query = f'''
+        SELECT
+            account_id,
+            SUM(amount) AS amount_sum_by_account
+        FROM {table_name}
+        GROUP BY account_id
+        ORDER BY account_id;
+        '''
+    df_amounts_by_account = select_data(sql_query)
+    df_amounts_by_account.set_index(
+        keys='account_id',
+        inplace=True)
+    
+    sql_query = f'''
+        SELECT
+            account_id,
+            entity_id,
+            SUM(amount) AS amount_sum_by_account_entity
+        FROM {table_name}
+        GROUP BY account_id, entity_id
+        ORDER BY account_id, entity_id;
+        '''
+    df_amounts_by_account_entity = select_data(sql_query)
+    df_amounts_by_account_entity.set_index(
+        keys=['account_id', 'entity_id'],
+        inplace=True)
 
-    df = select_data(sql_query)
-    return df
+    dfp = pd.pivot_table(
+        data=df_amounts,
+        values='amount_sum',
+        index=['account_id', 'entity_id'],
+        columns='period_name',
+        aggfunc='sum',
+        fill_value=0,
+        margins=False,
+        sort=False)
+
+    dfp = dfp.join(
+        other=df_amounts_by_account,
+        on='account_id',
+        validate='m:1')
+
+    dfp = dfp.join(
+        other=df_amounts_by_account_entity,
+        validate='m:1')
+
+    dfp.sort_values(
+        by=['amount_sum_by_account', 'amount_sum_by_account_entity'],
+        ascending=[False, False],
+        inplace=True)
+
+    dfp.reset_index(inplace=True)
+    dfp = dfp.merge(
+        right=df_accounts,
+        how='left',
+        left_on='account_id',
+        right_on='id',
+        validate='m:1')
+    dfp.entity_id = dfp.entity_id.astype('int')
+    dfp = dfp.merge(
+        right=df_entities,
+        how='left',
+        left_on='entity_id',
+        right_on='id',
+        validate='m:1')
+    dfp.drop(columns=['entity_id', 'id', 'amount_sum_by_account'],
+        inplace=True)
+
+    dfp_totals = dfp.sum(axis=0,
+        numeric_only=True).to_frame('Total').T
+    dfp = pd.concat([dfp_totals, dfp])
+    dfp.loc['Total'] = dfp.loc['Total'].fillna('')
+
+    dfp.sort_index(
+        axis=1,
+        inplace=True)
+
+    move_column(data_frame=dfp,
+        column_name='account_id',
+        loc=0)
+
+    move_column(data_frame=dfp,
+        column_name='account_name',
+        loc=1)
+
+    move_column(data_frame=dfp,
+        column_name='entity_1C_name',
+        loc=2)
+
+    move_column(data_frame=dfp,
+        column_name='inn',
+        loc=3)
+
+    return dfp
 
 
 def pivot_and_sort_data_frame_with_single_analytics(
@@ -226,51 +324,51 @@ def pivot_and_sort_data_frame_with_single_analytics(
     return dfp
 
 
-def pivot_and_sort_data_frame_with_two_analytics(
-        df: pd.core.frame.DataFrame) \
-        -> pd.core.frame.DataFrame:
-
-    beg_date, end_date = get_min_max_dates()
-    df_periods = get_periods(beg_date, end_date)
-
-    dfm = df_periods.merge(df, how='outer', on=['period_name'])
-    column_numbers = dfm.shape[1]
-    dfm.iloc[:, column_numbers - 5] = \
-        dfm.iloc[:, column_numbers - 5].fillna('Not specified')
-    dfm.iloc[:, column_numbers - 4] = \
-        dfm.iloc[:, column_numbers - 4].fillna('Not specified')
-    dfm.iloc[:, column_numbers - 3] = \
-        dfm.iloc[:, column_numbers - 3].fillna('Not specified')
-    dfm.iloc[:, column_numbers - 2] = \
-        dfm.iloc[:, column_numbers - 2].fillna('Not specified')
-    dfm.iloc[:, column_numbers - 1] = \
-        dfm.iloc[:, column_numbers - 1].fillna(0)
-
-    columns = dfm.columns.tolist()
-    periods = columns[0]
-    analytics1_id = columns[3]
-    analytics1_name = columns[4]
-    analytics2_id = columns[5]
-    analytics2_name = columns[6]
-    values = columns[7]
-
-    dfp = dfm.pivot_table(
-        index=[analytics1_id,
-            analytics1_name,
-            analytics2_id,
-            analytics2_name],
-        columns=periods,
-        values=values,
-        aggfunc='sum',
-        fill_value=0,
-        margins=True)
-    dfp = dfp.sort_values('All', ascending=False, axis=0)
-    # dfp = dfp.drop('All')
-
-    # remove rows with only zeroes
-    dfp = dfp[~(dfp == 0).all(axis=1)]
-
-    return dfp
+# def pivot_and_sort_data_frame_with_two_analytics(
+#         df: pd.core.frame.DataFrame) \
+#         -> pd.core.frame.DataFrame:
+# 
+#     beg_date, end_date = get_min_max_dates()
+#     df_periods = get_periods(beg_date, end_date)
+# 
+#     dfm = df_periods.merge(df, how='outer', on=['period_name'])
+#     column_numbers = dfm.shape[1]
+#     dfm.iloc[:, column_numbers - 5] = \
+#         dfm.iloc[:, column_numbers - 5].fillna('Not specified')
+#     dfm.iloc[:, column_numbers - 4] = \
+#         dfm.iloc[:, column_numbers - 4].fillna('Not specified')
+#     dfm.iloc[:, column_numbers - 3] = \
+#         dfm.iloc[:, column_numbers - 3].fillna('Not specified')
+#     dfm.iloc[:, column_numbers - 2] = \
+#         dfm.iloc[:, column_numbers - 2].fillna('Not specified')
+#     dfm.iloc[:, column_numbers - 1] = \
+#         dfm.iloc[:, column_numbers - 1].fillna(0)
+# 
+#     columns = dfm.columns.tolist()
+#     periods = columns[0]
+#     analytics1_id = columns[3]
+#     analytics1_name = columns[4]
+#     analytics2_id = columns[5]
+#     analytics2_name = columns[6]
+#     values = columns[7]
+# 
+#     dfp = dfm.pivot_table(
+#         index=[analytics1_id,
+#             analytics1_name,
+#             analytics2_id,
+#             analytics2_name],
+#         columns=periods,
+#         values=values,
+#         aggfunc='sum',
+#         fill_value=0,
+#         margins=True)
+#     dfp = dfp.sort_values('All', ascending=False, axis=0)
+#     # dfp = dfp.drop('All')
+# 
+#     # remove rows with only zeroes
+#     dfp = dfp[~(dfp == 0).all(axis=1)]
+# 
+#     return dfp
 
 
 def format_sheet(wb_path: str, sh_name: str, column_width: list[int]) -> None:
@@ -373,27 +471,47 @@ def cf_to_excel2(
         remove(file_path)
      
     activities = ['op', 'inv', 'fin', 'equity', 'vgo']
-    # activities = ['inv']
 
     for activity in activities:
         cf_ins = 'ins_' + activity
-        df_ins = select_function(cf_ins)
-        print(df_ins)
-        break
-        dfp_ins = pivot_and_sort_data_frame_with_two_analytics(df_ins)
-
+        # df_ins = select_function(cf_ins)
+        dfp_ins = select_function(cf_ins)
+        # dfp_ins = pd.pivot_table(
+        #         data=df_ins,
+        #         values='amount_sum',
+        #         index=['account_id', 'entity_id'],
+        #         columns='period_name',
+        #         aggfunc='sum',
+        #         fill_value=0,
+        #         margins=True,
+        #         sort=False)
+        # dfp_ins = pivot_and_sort_data_frame_with_two_analytics(df_ins)
+        # dfp_ins.sort_index(axis=1, inplace=True)
 
         writer_mode = 'a' if exists(file_path) else 'w'
         with pd.ExcelWriter(file_path, mode=writer_mode) as writer:  
             dfp_ins.to_excel(
                 writer,
                 sheet_name=activity,
+                float_format='%.2f',
+                index=False,
                 freeze_panes=(1, 4))
         
         cf_outs = 'outs_' + activity
-        df_outs = select_function(cf_outs)
-        print(df_outs)
-        dfp_outs = pivot_and_sort_data_frame_with_two_analytics(df_outs)
+        # df_outs = select_function(cf_outs)
+        dfp_outs = select_function(cf_outs)
+        # dfp_outs = pd.pivot_table(
+        #         data=df_outs,
+        #         values='amount_sum',
+        #         index=['account_id', 'entity_id'],
+        #         columns='period_name',
+        #         aggfunc='sum',
+        #         fill_value=0,
+        #         margins=True,
+        #         sort=False)
+        # dfp_outs = pivot_and_sort_data_frame_with_two_analytics(df_outs)
+        # dfp_outs.sort_index(axis=1, inplace=True)
+        
         start_row = writer.sheets[activity].max_row + 1
 
         writer_mode = 'a'
@@ -402,6 +520,8 @@ def cf_to_excel2(
             dfp_outs.to_excel(
                 writer,
                 sheet_name=activity,
+                float_format='%.2f',
+                index=False,
                 header=False,
                 startrow=start_row)
 
@@ -413,20 +533,20 @@ def cf_to_excel2(
 
 
 if __name__ == '__main__':
-    # file_name = 'cf_by_entity.xlsx'
-    # select_function = select_sum_amount_group_by_entity
-    # column_width = [32, 14, 18]
-    # cf_to_excel(select_function, file_name, column_width)
+    file_name = 'cf_by_entity.xlsx'
+    select_function = select_sum_amount_group_by_entity
+    column_width = [32, 14, 18]
+    cf_to_excel(select_function, file_name, column_width)
 
-    # file_name = 'cf_by_article.xlsx'
-    # select_function = select_sum_amount_group_by_article
-    # column_width = [32, 0, 18]
-    # cf_to_excel(select_function, file_name, column_width)
+    file_name = 'cf_by_article.xlsx'
+    select_function = select_sum_amount_group_by_article
+    column_width = [32, 0, 18]
+    cf_to_excel(select_function, file_name, column_width)
 
-    # file_name = 'cf_by_account.xlsx'
-    # select_function = select_sum_amount_group_by_account
-    # column_width = [8, 32, 18]
-    # cf_to_excel(select_function, file_name, column_width)
+    file_name = 'cf_by_account.xlsx'
+    select_function = select_sum_amount_group_by_account
+    column_width = [8, 32, 18]
+    cf_to_excel(select_function, file_name, column_width)
 
     file_name = 'cf_by_account_entity.xlsx'
     select_function = select_sum_amount_group_by_account_entity
